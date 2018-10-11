dc5113605523f77ea4bb6fd2e378064f89a69893
mi_shop-lt <email@mishop.com>
2018-10-11 11:56:14 +0800

    nav1子导航栏

--- js/nav1-child.js
+++ js/nav1-child.js
@@ -1,12 +1,42 @@
-var navChild1 = (function(){
+var navChild1 = (function () {
     return {
-        init: function(ele){
-            if(typeof ele == 'string'){
-                ele = document.querySelector(ele);
+        init: function (ele) {
+            this.$ele = document.querySelector(ele);
+            this.event();
+            this.getShopListData();
+        },
+        event: function () {
+            var _this = this;
+        },
+        // 获取所有商品信息
+        getShopListData: function () {
+            var _this = this;
+            var params = {
+                success: function (data) {
+                    data = JSON.parse(data);
+                    _this.shopList = data.data;
+                    _this.insertCarList(data.data);
+                }
             }
-            this.$ele = ele;
-            this.$listChild = document.querySelector('.list-child');
-            this.eevent();
-        }
+            sendAjax('json/navchild1.json', params);
+        },
+        // 把数据渲染到页面中
+        insertCarList: function (data) {
+            var arr = [];
+            var shop;
+            // debugger
+            for (var i = 0; i < data.length; i++) {
+                arr.push(`<li>
+                                <span>${data[i].ps}</span>
+                                <div>
+                                    <img src="${data[i].image}" alt="">
+                                    <em></em>
+                                </div>
+                                <a href="#">${data[i].name}</a>
+                                <p>${data[i].price}</p>
+                            </li>`);
+            }
+            this.$ele.innerHTML = arr.join('');
+        },
     }
 }())
