e956f591910bd6c8f94e56e0da3294e2e08c5c95
mi_shop-lt <email@mishop.com>
2018-10-12 21:12:10 +0800

    商品详情页的轮播和导航栏随屏幕滑动

--- js/swiper.js
+++ js/swiper.js
@@ -4,7 +4,7 @@ var mySwiper1 = new Swiper ('#swiper1', {
     loop: true, // 循环模式选项
     initialSlide: 0, //初始化页面索引
     // grabCursor:true, //鼠标变为手，点击图片时变为握手
-    // autoplay: true,
+    autoplay: true,
     effect: 'fade',
     pagination: {
         el: '.swiper-pagination',
@@ -16,3 +16,19 @@ var mySwiper1 = new Swiper ('#swiper1', {
         prevEl: '.swiper-button-prev',
       },
 }) 
+
+var mySwiper2 = new Swiper('#swiper2', {
+    direction: 'horizontal',
+    autoplay: true,//可选选项，自动滑动
+    effect: 'fade',
+    loop : true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        bulletElement : 'img',
+        clickable: true,
+    }, 
+})
